d98864af396fffd512308320dd31576c87b2d37a
Steven DR <145115889+sdr20@users.noreply.github.com>
2025-01-26 22:46:29 +0800

    Update FIreF.ino

--- FIreF.ino
+++ FIreF.ino
@@ -4,114 +4,111 @@
 #include <DHT_U.h>
 #include <SoftwareSerial.h>
 
-
 #define FLAME_SENSOR_PIN 5
 #define DHT_PIN 4
 #define DHT_TYPE DHT22
 #define RELAY_PIN 25
 #define BUZZER_PIN 26
 
-
 DHT dht(DHT_PIN, DHT_TYPE);
-
-
-SoftwareSerial gsmSerial(17, 16); 
-
+SoftwareSerial gsmSerial(17, 16);
 bool smsSent = false;
-
-
 unsigned long previousMillis = 0;
-int frequency = 500; 
+int frequency = 500;
 bool increasing = true;
 
 void setup() {
-
   Serial.begin(115200);
-
   gsmSerial.begin(9600);
-
-
+  Serial.println("Initializing...");
   dht.begin();
-
-  // Configure pin modes
   pinMode(FLAME_SENSOR_PIN, INPUT);
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH); 
-  digitalWrite(BUZZER_PIN, LOW); 
-
-
+  digitalWrite(RELAY_PIN, HIGH);
+  digitalWrite(BUZZER_PIN, LOW);
   delay(10000);
+  Serial.println("Testing GSM module...");
+  gsmSerial.println("AT");
+  delay(1000);
+  while (gsmSerial.available()) {
+    Serial.write(gsmSerial.read());
+  }
+  Serial.println("Checking SIM card presence...");
+  gsmSerial.println("AT+CSMINS?");
+  delay(1000);
+  while (gsmSerial.available()) {
+    Serial.write(gsmSerial.read());
+  }
+  Serial.println("Checking network registration...");
+  gsmSerial.println("AT+CREG?");
+  delay(1000);
+  while (gsmSerial.available()) {
+    Serial.write(gsmSerial.read());
+  }
+  Serial.println("GSM initialization complete.");
 }
 
 void loop() {
   int val = digitalRead(FLAME_SENSOR_PIN);
-
-
   Serial.print("Flame Sensor Value: ");
   Serial.println(val);
 
-  if (val == HIGH) { 
-
+  if (val == HIGH) {
     Serial.println("Fire detected!");
-
-
     digitalWrite(RELAY_PIN, LOW);
-
-
     unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= 10) { 
+    if (currentMillis - previousMillis >= 10) {
       previousMillis = currentMillis;
       if (increasing) {
         frequency += 10;
-        if (frequency >= 1000) { 
+        if (frequency >= 1000) {
           increasing = false;
         }
       } else {
         frequency -= 10;
-        if (frequency <= 500) { 
+        if (frequency <= 500) {
           increasing = true;
         }
       }
       tone(BUZZER_PIN, frequency);
     }
 
-
     if (!smsSent) {
-
       float temperature = dht.readTemperature();
       float humidity = dht.readHumidity();
-
- 
       String message = "Warning! Fire detected! Temp: " + String(temperature) + "C, Humidity: " + String(humidity) + "%";
-
-      sendSMS("09999", message);
-
+      sendSMS("09751969408", message);
       smsSent = true;
     }
 
-  } else { 
-
+  } else {
     Serial.println("No fire detected.");
-
-
     digitalWrite(RELAY_PIN, HIGH);
     noTone(BUZZER_PIN);
-
-
     smsSent = false;
   }
-
   delay(100);
 }
 
 void sendSMS(String phoneNumber, String message) {
-  gsmSerial.println("AT+CMGF=1"); 
+  Serial.println("Sending SMS...");
+  gsmSerial.println("AT+CMGF=1");
   delay(1000);
-  gsmSerial.println("AT+CMGS=\"" + phoneNumber + "\""); 
+  while (gsmSerial.available()) {
+    Serial.write(gsmSerial.read());
+  }
+  gsmSerial.println("AT+CMGS=\"" + phoneNumber + "\"");
   delay(1000);
+  while (gsmSerial.available()) {
+    Serial.write(gsmSerial.read());
+  }
   gsmSerial.print(message);
   delay(1000);
-  gsmSerial.write(26); 
-  delay(1000);
+  gsmSerial.write(26);
+  delay(3000);
+  while (gsmSerial.available()) {
+    Serial.write(gsmSerial.read());
+  }
+  Serial.println("SMS sent.");
 }
